ecf581b7f6d9bba0dcff1003c2d9f12a19218671
Sheng Yang <sheng@yasker.org>
2017-03-25 22:31:08 -0700

    fsops: Abstruct VFS into FileSystemOps

--- vfs/vfs.go
+++ vfs/vfs.go
@@ -2,15 +2,12 @@ package vfs
 
 import (
 	"fmt"
-	"io"
 	"net/url"
-	"os"
 	"path/filepath"
-	"strings"
 
 	"github.com/Sirupsen/logrus"
 	"github.com/yasker/backupstore"
-	"github.com/yasker/backupstore/util"
+	"github.com/yasker/backupstore/fsops"
 )
 
 var (
@@ -20,14 +17,14 @@ var (
 type BackupStoreDriver struct {
 	destURL string
 	path    string
+
+	*fsops.FileSystemOperator
 }
 
 const (
 	KIND = "vfs"
 
 	VfsPath = "vfs.path"
-
-	MaxCleanupLevel = 10
 )
 
 func init() {
@@ -38,6 +35,8 @@ func init() {
 
 func initFunc(destURL string) (backupstore.BackupStoreDriver, error) {
 	b := &BackupStoreDriver{}
+	b.FileSystemOperator = fsops.NewFileSystemOperator(b)
+
 	u, err := url.Parse(destURL)
 	if err != nil {
 		return nil, err
@@ -65,15 +64,8 @@ func initFunc(destURL string) (backupstore.BackupStoreDriver, error) {
 	return b, nil
 }
 
-func (v *BackupStoreDriver) updatePath(path string) string {
+func (v *BackupStoreDriver) LocalPath(path string) string {
 	return filepath.Join(v.path, path)
-}
-
-func (v *BackupStoreDriver) preparePath(file string) error {
-	if err := os.MkdirAll(filepath.Dir(v.updatePath(file)), os.ModeDir|0700); err != nil {
-		return err
-	}
-	return nil
 }
 
 func (v *BackupStoreDriver) Kind() string {
@@ -82,111 +74,4 @@ func (v *BackupStoreDriver) Kind() string {
 
 func (v *BackupStoreDriver) GetURL() string {
 	return v.destURL
-}
-
-func (v *BackupStoreDriver) FileSize(filePath string) int64 {
-	file := v.updatePath(filePath)
-	st, err := os.Stat(file)
-	if err != nil || st.IsDir() {
-		return -1
-	}
-	return st.Size()
-}
-
-func (v *BackupStoreDriver) FileExists(filePath string) bool {
-	return v.FileSize(filePath) >= 0
-}
-
-func (v *BackupStoreDriver) Remove(names ...string) error {
-	for _, name := range names {
-		if err := os.RemoveAll(v.updatePath(name)); err != nil {
-			return err
-		}
-		//Also automatically cleanup upper level directories
-		dir := v.updatePath(name)
-		for i := 0; i < MaxCleanupLevel; i++ {
-			dir = filepath.Dir(dir)
-			// Don't clean above backupstore base
-			if strings.HasSuffix(dir, backupstore.GetBackupstoreBase()) {
-				break
-			}
-			// If directory is not empty, then we don't need to continue
-			if err := os.Remove(dir); err != nil {
-				break
-			}
-		}
-	}
-	return nil
-}
-
-func (v *BackupStoreDriver) Read(src string) (io.ReadCloser, error) {
-	file, err := os.Open(v.updatePath(src))
-	if err != nil {
-		return nil, err
-	}
-	return file, nil
-}
-
-func (v *BackupStoreDriver) Write(dst string, rs io.ReadSeeker) error {
-	tmpFile := dst + ".tmp"
-	if v.FileExists(tmpFile) {
-		v.Remove(tmpFile)
-	}
-	if err := v.preparePath(dst); err != nil {
-		return err
-	}
-	file, err := os.Create(v.updatePath(tmpFile))
-	if err != nil {
-		return err
-	}
-	defer file.Close()
-	_, err = io.Copy(file, rs)
-	if err != nil {
-		return err
-	}
-
-	if v.FileExists(dst) {
-		v.Remove(dst)
-	}
-	return os.Rename(v.updatePath(tmpFile), v.updatePath(dst))
-}
-
-func (v *BackupStoreDriver) List(path string) ([]string, error) {
-	out, err := util.Execute("ls", []string{"-1", v.updatePath(path)})
-	if err != nil {
-		return nil, err
-	}
-	var result []string
-	if len(out) == 0 {
-		return result, nil
-	}
-	result = strings.Split(strings.TrimSpace(string(out)), "\n")
-	return result, nil
-}
-
-func (v *BackupStoreDriver) Upload(src, dst string) error {
-	tmpDst := dst + ".tmp"
-	if v.FileExists(tmpDst) {
-		v.Remove(tmpDst)
-	}
-	if err := v.preparePath(dst); err != nil {
-		return err
-	}
-	_, err := util.Execute("cp", []string{src, v.updatePath(tmpDst)})
-	if err != nil {
-		return err
-	}
-	_, err = util.Execute("mv", []string{v.updatePath(tmpDst), v.updatePath(dst)})
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-func (v *BackupStoreDriver) Download(src, dst string) error {
-	_, err := util.Execute("cp", []string{v.updatePath(src), dst})
-	if err != nil {
-		return err
-	}
-	return nil
 }
