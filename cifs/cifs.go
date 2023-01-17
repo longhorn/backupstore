7373a820e90dca0658e9b694b4ae78e3322e9346
Derek Su <derek.su@suse.com>
2023-01-17 21:20:52 +0800

    cifs: use mount-utils instead
    Longhorn 3599
    
    Signed-off-by: Derek Su <derek.su@suse.com>

--- cifs/cifs.go
+++ cifs/cifs.go
@@ -11,6 +11,9 @@ import (
 	"github.com/pkg/errors"
 	"github.com/sirupsen/logrus"
 
+	"k8s.io/apimachinery/pkg/util/wait"
+	mount "k8s.io/mount-utils"
+
 	"github.com/longhorn/backupstore"
 	"github.com/longhorn/backupstore/fsops"
 	"github.com/longhorn/backupstore/util"
@@ -65,48 +68,57 @@ func initFunc(destURL string) (backupstore.BackupStoreDriver, error) {
 		return nil, fmt.Errorf("cannot find CIFS path")
 	}
 
+	b.username = os.Getenv("CIFS_USERNAME")
+	b.password = os.Getenv("CIFS_PASSWORD")
 	b.serverPath = u.Host + u.Path
 	b.mountDir = filepath.Join(MountDir, strings.TrimRight(strings.Replace(u.Host, ".", "_", -1), ":"), u.Path)
+	b.destURL = KIND + "://" + b.serverPath
+
 	if _, err = util.ExecuteWithCustomTimeout("mkdir", []string{"-m", "700", "-p", b.mountDir}, defaultTimeout); err != nil {
 		return nil, errors.Wrapf(err, "cannot create mount directory %v for CIFS server", b.mountDir)
 	}
 
-	b.username = os.Getenv("CIFS_USERNAME")
-	b.password = os.Getenv("CIFS_PASSWORD")
-
 	if err := b.mount(); err != nil {
-		return nil, errors.Wrapf(err, "cannot mount CIFS %v", b.serverPath)
-	}
-	if _, err := b.List(""); err != nil {
-		return nil, fmt.Errorf("CIFS path %v doesn't exist or is not a directory", b.serverPath)
+		return nil, errors.Wrapf(err, "cannot mount CIFS share %v", b.serverPath)
 	}
 
-	b.destURL = KIND + "://" + b.serverPath
+	if _, err := b.List(""); err != nil {
+		return nil, errors.Wrapf(err, "CIFS path %v doesn't exist or is not a directory", b.serverPath)
+	}
+
 	log.Infof("Loaded driver for %v", b.destURL)
+
 	return b, nil
 }
 
-func (b *BackupStoreDriver) mount() (err error) {
-	if util.IsMounted(b.mountDir) {
+func (b *BackupStoreDriver) mount() error {
+	mounter := mount.NewWithoutSystemd("")
+
+	if mounted, err := mounter.IsMountPoint(b.mountDir); err != nil {
+		return err
+	} else if mounted {
+		log.Debugf("CIFS share %v is already mounted on %v", b.destURL, b.mountDir)
 		return nil
 	}
 
-	cmd := []string{
-		"-t", "cifs",
+	mountOptions := []string{}
+	sensitiveMountOptions := []string{
+		fmt.Sprintf("username=%v", b.username),
+		fmt.Sprintf("password=%v", b.password),
 	}
-	if b.username != "" {
-		cmd = append(cmd, "-o", fmt.Sprintf("username=%v", b.username))
-	}
-	if b.password != "" {
-		cmd = append(cmd, "-o", fmt.Sprintf("password=%v", b.password))
-	}
-	cmd = append(cmd, "//"+b.serverPath, b.mountDir)
 
-	_, err = util.ExecuteWithCustomTimeout("mount", cmd, defaultTimeout)
-	if err != nil {
-		return errors.Wrapf(err, "failed to mount using CIFS")
+	log.Infof("Mounting CIFS share %v on mount point %v with options %+v", b.destURL, b.mountDir, mountOptions)
+
+	mountComplete := false
+	err := wait.PollImmediate(1*time.Second, defaultTimeout, func() (bool, error) {
+		err := mounter.MountSensitiveWithoutSystemd("//"+b.serverPath, b.mountDir, KIND, mountOptions, sensitiveMountOptions)
+		mountComplete = true
+		return true, err
+	})
+	if !mountComplete {
+		return errors.Wrapf(err, "mounting CIFS share %v on %v timed out", b.destURL, b.mountDir)
 	}
-	return nil
+	return err
 }
 
 func (b *BackupStoreDriver) Kind() string {
--- go.mod
+++ go.mod
@@ -10,19 +10,27 @@ require (
 	github.com/pkg/errors v0.9.1
 	github.com/sirupsen/logrus v1.3.0
 	github.com/spf13/afero v1.5.1
-	github.com/stretchr/testify v1.7.0
+	github.com/stretchr/testify v1.8.0
 	github.com/urfave/cli v1.14.0
-	golang.org/x/net v0.0.0-20200202094626-16171245cfb2
-	gopkg.in/check.v1 v1.0.0-20160105164936-4f90aeace3a2
+	golang.org/x/net v0.3.1-0.20221206200815-1e63c2f08a10
+	gopkg.in/check.v1 v1.0.0-20200227125254-8fa46927fb4f
+	k8s.io/apimachinery v0.26.0
+	k8s.io/mount-utils v0.26.0
 )
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/go-logr/logr v1.2.3 // indirect
 	github.com/jmespath/go-jmespath v0.3.0 // indirect
 	github.com/konsorten/go-windows-terminal-sequences v1.0.1 // indirect
+	github.com/kr/text v0.2.0 // indirect
+	github.com/moby/sys/mountinfo v0.6.2 // indirect
+	github.com/niemeyer/pretty v0.0.0-20200227124842-a10e7caefd8e // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	golang.org/x/crypto v0.0.0-20190820162420-60c769a6c586 // indirect
-	golang.org/x/sys v0.0.0-20190412213103-97732733099d // indirect
-	golang.org/x/text v0.3.3 // indirect
-	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
+	golang.org/x/sys v0.3.0 // indirect
+	golang.org/x/text v0.5.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+	k8s.io/klog/v2 v2.80.1 // indirect
+	k8s.io/utils v0.0.0-20221107191617-1a15be271d1d // indirect
 )
