2c8b5eb0e646cd4e8f5b80f922f9c150f584d875
Derek Su <derek.su@suse.com>
2023-03-25 15:01:15 +0800

    Clean up outdated or corrupted mountpoint
    Longhorn 5654
    
    Signed-off-by: Derek Su <derek.su@suse.com>

--- go.mod
+++ go.mod
@@ -4,31 +4,35 @@ go 1.17
 
 require (
 	github.com/aws/aws-sdk-go v1.34.2
+	github.com/google/fscrypt v0.3.3
 	github.com/google/uuid v1.3.0
 	github.com/honestbee/jobq v1.0.2
 	github.com/pkg/errors v0.9.1
 	github.com/sirupsen/logrus v1.3.0
 	github.com/spf13/afero v1.5.1
 	github.com/stretchr/testify v1.8.0
-	github.com/urfave/cli v1.14.0
+	github.com/urfave/cli v1.20.0
 	golang.org/x/net v0.7.0
 	gopkg.in/check.v1 v1.0.0-20200227125254-8fa46927fb4f
-	k8s.io/apimachinery v0.26.3
-	k8s.io/mount-utils v0.26.3
+	k8s.io/apimachinery v0.26.0
+	k8s.io/mount-utils v0.26.0
 )
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/go-logr/logr v1.2.3 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/jmespath/go-jmespath v0.3.0 // indirect
 	github.com/konsorten/go-windows-terminal-sequences v1.0.1 // indirect
 	github.com/kr/text v0.2.0 // indirect
 	github.com/moby/sys/mountinfo v0.6.2 // indirect
 	github.com/niemeyer/pretty v0.0.0-20200227124842-a10e7caefd8e // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	golang.org/x/crypto v0.0.0-20190820162420-60c769a6c586 // indirect
+	golang.org/x/crypto v0.0.0-20220408190544-5352b0902921 // indirect
 	golang.org/x/sys v0.5.0 // indirect
+	golang.org/x/term v0.5.0 // indirect
 	golang.org/x/text v0.7.0 // indirect
+	google.golang.org/protobuf v1.28.1 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 	k8s.io/klog/v2 v2.80.1 // indirect
 	k8s.io/utils v0.0.0-20221107191617-1a15be271d1d // indirect
--- nfs/nfs.go
+++ nfs/nfs.go
@@ -61,17 +61,15 @@ func initFunc(destURL string) (backupstore.BackupStoreDriver, error) {
 		return nil, fmt.Errorf("BUG: Why dispatch %v to %v?", u.Scheme, KIND)
 	}
 	if u.Host == "" {
-		return nil, fmt.Errorf("NFS path must follow: nfs://server:/path/ format")
+		return nil, fmt.Errorf("NFS path must follow format: nfs://<server-address>:/<share-name>/")
 	}
 	if u.Path == "" {
 		return nil, fmt.Errorf("cannot find nfs path")
 	}
 
 	b.serverPath = u.Host + u.Path
+	b.destURL = KIND + "://" + b.serverPath
 	b.mountDir = filepath.Join(MountDir, strings.TrimRight(strings.Replace(u.Host, ".", "_", -1), ":"), u.Path)
-	if _, err = util.ExecuteWithCustomTimeout("mkdir", []string{"-m", "700", "-p", b.mountDir}, defaultTimeout); err != nil {
-		return nil, errors.Wrapf(err, "cannot create mount directory %v for NFS server", b.mountDir)
-	}
 
 	if err := b.mount(); err != nil {
 		return nil, errors.Wrapf(err, "cannot mount nfs %v", b.serverPath)
@@ -80,22 +78,23 @@ func initFunc(destURL string) (backupstore.BackupStoreDriver, error) {
 		return nil, fmt.Errorf("NFS path %v doesn't exist or is not a directory", b.serverPath)
 	}
 
-	b.destURL = KIND + "://" + b.serverPath
 	log.Infof("Loaded driver for %v", b.destURL)
+
 	return b, nil
 }
 
-func (b *BackupStoreDriver) mount() (err error) {
+func (b *BackupStoreDriver) mount() error {
 	mounter := mount.NewWithoutSystemd("")
 
-	if mounted, err := mounter.IsMountPoint(b.mountDir); err != nil {
+	mounted, err := util.EnsureMountPoint(KIND, b.mountDir, mounter, log)
+	if err != nil {
 		return err
-	} else if mounted {
-		log.Debugf("NFS share %v is already mounted on %v", b.destURL, b.mountDir)
+	}
+	if mounted {
 		return nil
 	}
 
-	retErr := errors.New("Cannot mount using NFSv4")
+	retErr := errors.New("cannot mount using NFSv4")
 
 	for _, version := range MinorVersions {
 		log.Debugf("Attempting mount for nfs path %v with nfsvers %v", b.serverPath, version)
@@ -103,6 +102,9 @@ func (b *BackupStoreDriver) mount() (err error) {
 		mountOptions := []string{
 			fmt.Sprintf("nfsvers=%v", version),
 			"actimeo=1",
+			"soft",
+			"timeo=300",
+			"retry=2",
 		}
 		sensitiveMountOptions := []string{}
 
