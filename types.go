ea85db1b92a8bb0f138947391d58a023c1a5ed19
Derek Su <derek.su@suse.com>
2023-05-02 21:22:51 +0800

    Remove "starting" phase
    Longhorn 5662
    
    Signed-off-by: Derek Su <derek.su@suse.com>
    (cherry picked from commit d9dec199a706b1549b3878c147cfe803ba8dde5a)

--- types.go
+++ types.go
@@ -3,7 +3,6 @@ package backupstore
 type ProgressState string
 
 const (
-	ProgressStateStarting   = ProgressState("starting")
 	ProgressStateInProgress = ProgressState("in_progress")
 	ProgressStateComplete   = ProgressState("complete")
 	ProgressStateError      = ProgressState("error")
