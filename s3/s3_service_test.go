b4df0fdc80bd910073aa51503414805f112a3b11
James Lu <james.lu@suse.com>
2026-03-04 10:45:10 +0800

    fix(s3): invalidRequest compatible error
    When the uploading file size exceeds 300MiB, it will use the
    multipart method to upload the file with the http header
    `STREAMING-AWS4-HMAC-SHA256-PAYLOAD-XXX`, and the header will cause
    compatible issues for some S3-compatible services.
    
    ref: longhorn/longhorn 12713
    
    Signed-off-by: James Lu <james.lu@suse.com>

--- s3/s3_service_test.go
+++ s3/s3_service_test.go
@@ -4,7 +4,9 @@ package s3
 
 import (
 	"bytes"
+	"context"
 	"io"
+	"net/url"
 	"os"
 	"testing"
 
@@ -16,22 +18,32 @@ import (
 func Test(t *testing.T) { TestingT(t) }
 
 type TestSuite struct {
-	service S3Service
+	service *service
 }
 
 var _ = Suite(&TestSuite{})
 
+// Before running the test, make sure to set the following environment variables for S3 test:
+// export CONVOY_TEST_BACKUPTARGET_URL="s3://bucket_name@region/"
+// export AWS_ACCESS_KEY_ID=""
+// export AWS_SECRET_ACCESS_KEY=""
+// export AWS_ENDPOINTS=""
+// Command: go test -tags=s3test ./s3/
+
 const (
-	ENV_TEST_AWS_REGION = "CONVOY_TEST_AWS_REGION"
-	ENV_TEST_AWS_BUCKET = "CONVOY_TEST_AWS_BUCKET"
+	ENV_TEST_BACKUPTARGET_URL = "CONVOY_TEST_BACKUPTARGET_URL"
 )
 
 func (s *TestSuite) SetUpSuite(c *C) {
 	logrus.SetLevel(logrus.DebugLevel)
 	logrus.SetOutput(os.Stderr)
 
-	s.service.Region = os.Getenv(ENV_TEST_AWS_REGION)
-	s.service.Bucket = os.Getenv(ENV_TEST_AWS_BUCKET)
+	destURL := os.Getenv(ENV_TEST_BACKUPTARGET_URL)
+	u, err := url.Parse(destURL)
+	c.Assert(err, IsNil)
+
+	s.service, err = newService(u)
+	c.Assert(err, IsNil)
 
 	if s.service.Region == "" || s.service.Bucket == "" {
 		c.Skip("S3 test environment variables not provided.")
@@ -40,38 +52,40 @@ func (s *TestSuite) SetUpSuite(c *C) {
 
 func (s *TestSuite) TestFuncs(c *C) {
 	var err error
+	ctx := context.Background()
 	body := []byte("this is only a test file")
 
 	key := "test_file"
 	key1 := "test_file_1"
 	key2 := "test_file_2"
 
-	err = s.service.PutObject(key1, bytes.NewReader(body))
+	err = s.service.PutObject(ctx, key1, bytes.NewReader(body))
 	c.Assert(err, IsNil)
-	err = s.service.PutObject(key2, bytes.NewReader(body))
+	err = s.service.PutObject(ctx, key2, bytes.NewReader(body))
 	c.Assert(err, IsNil)
 
-	objs, _, err := s.service.ListObjects(key, "")
+	objs, _, err := s.service.ListObjects(ctx, key, "")
 	c.Assert(err, IsNil)
 	c.Assert(objs, HasLen, 2)
 
-	r, err := s.service.GetObject(key1)
+	r, err := s.service.GetObject(ctx, key1)
 	c.Assert(err, IsNil)
 
 	newBody, err := io.ReadAll(r)
 	c.Assert(err, IsNil)
 	c.Assert(newBody, DeepEquals, body)
 
-	err = s.service.DeleteObjects([]string{key})
+	err = s.service.DeleteObjects(ctx, key)
 	c.Assert(err, IsNil)
 
-	objs, _, err = s.service.ListObjects(key, "")
+	objs, _, err = s.service.ListObjects(ctx, key, "")
 	c.Assert(err, IsNil)
 	c.Assert(objs, HasLen, 0)
 }
 
 func (s *TestSuite) TestList(c *C) {
 	var err error
+	ctx := context.Background()
 
 	body := []byte("this is only a test file")
 	dir1_key1 := "dir/dir1/test_file_1"
@@ -79,30 +93,59 @@ func (s *TestSuite) TestList(c *C) {
 	dir2_key1 := "dir/dir2/test_file_1"
 	dir2_key2 := "dir/dir2/test_file_2"
 
-	err = s.service.PutObject(dir1_key1, bytes.NewReader(body))
+	err = s.service.PutObject(ctx, dir1_key1, bytes.NewReader(body))
 	c.Assert(err, IsNil)
-	err = s.service.PutObject(dir1_key2, bytes.NewReader(body))
+	err = s.service.PutObject(ctx, dir1_key2, bytes.NewReader(body))
 	c.Assert(err, IsNil)
-	err = s.service.PutObject(dir2_key1, bytes.NewReader(body))
+	err = s.service.PutObject(ctx, dir2_key1, bytes.NewReader(body))
 	c.Assert(err, IsNil)
-	err = s.service.PutObject(dir2_key2, bytes.NewReader(body))
+	err = s.service.PutObject(ctx, dir2_key2, bytes.NewReader(body))
 	c.Assert(err, IsNil)
 
-	objs, prefixes, err := s.service.ListObjects("dir/", "/")
+	objs, prefixes, err := s.service.ListObjects(ctx, "dir/", "/")
 	c.Assert(err, IsNil)
 	c.Assert(objs, HasLen, 0)
 	c.Assert(prefixes, HasLen, 2)
 
-	objs, prefixes, err = s.service.ListObjects("dir/dir1/", "/")
+	objs, prefixes, err = s.service.ListObjects(ctx, "dir/dir1/", "/")
 	c.Assert(err, IsNil)
 	c.Assert(objs, HasLen, 2)
 	c.Assert(prefixes, HasLen, 0)
 
-	err = s.service.DeleteObjects([]string{"dir"})
+	err = s.service.DeleteObjects(ctx, "dir")
 	c.Assert(err, IsNil)
 
-	objs, prefixes, err = s.service.ListObjects("dir/", "")
+	objs, prefixes, err = s.service.ListObjects(ctx, "dir/", "")
 	c.Assert(err, IsNil)
 	c.Assert(objs, HasLen, 0)
 	c.Assert(prefixes, HasLen, 0)
+}
+
+func (s *TestSuite) TestPutObjectSinglePart(c *C) {
+	var err error
+	ctx := context.Background()
+	body := []byte("this is a test file for single part upload")
+	key := "test_single_part_file"
+
+	// Create an S3 client
+	svc, err := s.service.newInstance(ctx, false)
+	c.Assert(err, IsNil)
+	defer s.service.Close()
+
+	// Test successful single part upload
+	err = s.service.PutObjectSinglePart(ctx, svc, key, bytes.NewReader(body))
+	c.Assert(err, IsNil)
+
+	// Verify the object was uploaded by retrieving it
+	r, err := s.service.GetObject(ctx, key)
+	c.Assert(err, IsNil)
+	defer r.Close()
+
+	retrievedBody, err := io.ReadAll(r)
+	c.Assert(err, IsNil)
+	c.Assert(retrievedBody, DeepEquals, body)
+
+	// Clean up
+	err = s.service.DeleteObjects(ctx, key)
+	c.Assert(err, IsNil)
 }
